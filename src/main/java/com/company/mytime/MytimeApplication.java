9ab343d5e7f5be09c2a699c49bee1a3f4e6c4d62
Rajesh <Rajesh_Meda@gap.com>
2017-11-21 14:36:11 +0530

    First commit

---
+++ src/main/java/com/company/mytime/MytimeApplication.java
@@ -0,0 +1,12 @@
+package com.company.mytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytimeApplication.class, args);
+	}
+}
